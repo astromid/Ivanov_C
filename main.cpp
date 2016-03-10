47623d5f49896bab1d6bcf2b7f01cef27fe42f49
astromid <astromid@gmail.com>
2016-03-10 15:14:39 +0300

    Forward + bidirectional iterators implemented, but there are few bugs

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include "singly_linked_list.cpp"
 
-using namespace std;
-
 int main()
 {
 	List<int> NewList1;
-	NewList1.append(10);
 	NewList1.append(20);
 	NewList1.append(30);
-	cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << endl;
-	NewList1.insert(5, 0);
+	NewList1.append(40);
+	//cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << endl;
+	NewList1.insert(10, 0);
 	cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << " " << NewList1[3] << endl;
-	NewList1.remove(1);
-	cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << endl;
+	//NewList1.remove(1);
+	//cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << endl;
+
+	List<int> ::bid_list_iterator bIterator1;
+	List<int> ::bid_list_iterator bIterator2;
+
+	bIterator1 = NewList1.get_head();
+	bIterator2 = NewList1.get_head();
+	bIterator2 = NewList1.get_end();
+	reverse(bIterator1, bIterator2);
+	cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << " " << NewList1[3] << endl;
+	//cout << NewList1[0] << " " << NewList1[1] << " " << NewList1[2] << endl;
 	system("pause");
 	return 0;
 }
--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -1,3 +1,7 @@
+#include <iterator>
+
+using namespace std;
+
 template <typename T> struct node
 {
 	T data;
@@ -11,6 +15,109 @@ template <typename T> class List
 	int len;
 
 public:
+	class f_list_iterator : public iterator<forward_iterator_tag, T, ptrdiff_t, T*, T&>
+	{
+	public:
+		node<T>* f_iterator;
+		node<T>* f_iterator_head;
+
+		f_list_iterator()	//constructor
+		{
+			this->f_iterator = nullptr;
+			this->f_iterator_head = nullptr;
+		}
+
+		f_list_iterator(const f_list_iterator& fIt)	//copy-constructor
+		{
+			this->f_iterator = fIt.f_iterator;
+			this->f_iterator_head = fIt.f_iterator_head;
+		}
+
+		bool operator!=(f_list_iterator fIt)
+		{
+			if (this->f_iterator->next != fIt.f_iterator->next)
+			{
+				return true;
+			}
+			else return false;
+		}
+
+		bool operator==(f_list_iterator fIt)
+		{
+			if (this->f_iterator->next == fIt.f_iterator->next)
+			{
+				return true;
+			}
+			else return false;
+		}
+
+		f_list_iterator& operator++()
+		{
+			if (this->f_iterator->next != nullptr)
+			{
+				this->f_iterator = (this->f_iterator)->next;
+				return *this;
+			}
+			else return *this;
+		}
+
+		T& operator*()
+		{
+			return this->f_iterator->data;
+		}
+		
+		void operator=(f_list_iterator fIt)
+		{
+			this->f_iterator = fIt->f_iterator;
+		}
+
+		void operator=(node<T>* pointer)
+		{
+			this->f_iterator = pointer;
+		}
+	};
+
+	class bid_list_iterator : public f_list_iterator
+	{
+	public:
+
+		typedef bidirectional_iterator_tag iterator_category;
+		typedef T value_type;
+		typedef ptrdiff_t difference_type;
+		typedef T* pointer;
+		typedef T& reference;
+
+		void operator=(const bid_list_iterator& bIt)
+		{
+			this->f_iterator = bIt->f_iterator;
+			this->f_iterator_head = bIt->f_iterator_head;
+		}
+
+		void operator=(node<T>* pointer)
+		{
+			this->f_iterator = pointer;
+			if (this->f_iterator_head == nullptr)
+			{
+				this->f_iterator_head = pointer;
+			}
+		}
+
+		bid_list_iterator& operator--()
+		{
+			if (this->f_iterator != this->f_iterator_head)
+			{
+				node<T>* tmp = new node<T>;
+				tmp = this->f_iterator;
+				this->f_iterator = this->f_iterator_head;
+				while (this->f_iterator->next != tmp)
+				{
+					this->f_iterator++;
+				}
+				return *this;
+			}
+			else return *this;
+		}
+	};
 
 	List()
 	{
@@ -27,6 +134,16 @@ public:
 			delete head;
 			head = tmp;
 		}
+	}
+
+	node<T>* get_head()
+	{
+		return head;
+	}
+
+	node<T>* get_end()
+	{
+		return end;
 	}
 
 	void append(T new_element)	//add node in the end of the list
